916f0277a8fd1d2822ca3f39e409d592a5537385
ahmadwhet <36060669+ahmadwhet@users.noreply.github.com>
2024-09-21 10:06:22 +0700

    update requirements
    last update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# netflix-checker
-netflix cookies checker
+# Netflix Cookies Checker
+This tools helps you automate the process of checking if a Netflix cookie is valid or not saving you time and effort. Simply put all of your cookies in Netscape or JSON format (.txt | .json) in the `cookies` directory then run the script. It will check all of the cookies and output the valid ones in a new folder called `hits`, and the invalid ones in a new folder called `failures`.
+
+## How to run the script?
+1. Install the required packages by running `pip install -r requirements.txt`.
+2. A new directory called `cookies` will automatically be created. Put all of your cookies in it.
+3. Run the script by running `python main.py`.
+
+> [!NOTE]
+> No proxy is needed. Also, this script supports both Netscape and JSON format now.
+
+Keep in mind that the cookies will be renamed then moved to their respective folders. If the cookie is valid, the script will try to extract 4 things:
+- The country of sign up. (e.g. US, BR, MX, etc.).
+- Does the account have extra members feature. (e.g. True, None).
+- Account GUID.
+- Member since date.
+
+> [!TIP]
+> By default, the script automatically handles duplicates since it names working cookies in this format `cookie_` + `_country_` + `_extra(True/None)_` + `_guid_`. So basically, any duplicates will be overitten.
+> Also, this script supports multi-threading.
+
+## Disclaimer
+This tool was created for educational purposes. Use it at your own risk.
---
+++ utils.py
@@ -0,0 +1,50 @@
+import colorama
+import os
+import json
+import shutil
+
+def print_banner():
+    print(colorama.Fore.RED + """
+  _   _      _    __ _ _         _____            _    _           
+ | \\ | |    | |  / _| (_)       / ____|          | |  (_)          
+ |  \\| | ___| |_| |_| |___  __ | |     ___   ___ | | ___  ___  ___ 
+ | . ` |/ _ \\ __|  _| | \\ \\/ / | |    / _ \\ / _ \\| |/ / |/ _ \\/ __|
+ | |\\  |  __/ |_| | | | |>  <  | |___| (_) | (_) |   <| |  __/\\__ |
+ |_| \\_|\\___|\\__|_|_|_|_/_/\\_\\  \\_____\\___/ \\___/|_|\\_\\_|\\___||___/
+                / ____| |             | |                          
+               | |    | |__   ___  ___| | _____ _ __               
+               | |    | '_ \\ / _ \\/ __| |/ / _ \\ '__|              
+               | |____| | | |  __/ (__|   <  __/ |                 
+                \\_____|_| |_|\\___|\\___|_|\\_\\___|_|                                                        
+    """)
+    print("-------------------------------------------------------------------" + colorama.Fore.RESET)
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def convert_to_netscape_format(cookie):
+    """ Convert the cookie dictionary to the Netscape cookie format string """
+    flag = cookie.get('flag', '').upper()
+    return "{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+        cookie['domain'], 'TRUE' if flag == 'TRUE' else 'FALSE', cookie['path'],
+        'TRUE' if cookie.get('secure', False) else 'FALSE', cookie.get('expiration', ''), cookie['name'], cookie['value']
+    )
+
+def process_json_files(directory):
+    """ Process JSON files, convert them to Netscape format, and move the originals to a different folder """
+    json_after_conversion_folder = "json_cookies_after_conversion"  # Directory to move JSON cookies after conversion
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            os.makedirs(json_after_conversion_folder, exist_ok=True)
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as file:
+                try:
+                    cookies = json.load(file)
+                    if isinstance(cookies, list) and all('domain' in cookie for cookie in cookies):
+                        netscape_cookie_file = os.path.join(directory, filename.replace('.json', '.txt'))
+                        with open(netscape_cookie_file, 'w') as outfile:
+                            outfile.writelines([convert_to_netscape_format(cookie) + '\n' for cookie in cookies])
+                        shutil.move(file_path, os.path.join(json_after_conversion_folder, filename))
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON from file {filename}")
